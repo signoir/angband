0e5b92ddfcfbe3d71df99b9ba402704f2d0af1a4
anonymoussc <50c5ac69@opayq.com>
2019-01-21 11:43:33 +0700

    chore(tools): update tools

--- tools/test/jest.setup.ts
+++ tools/test/jest.setup.ts
@@ -1,22 +1,23 @@
-import 'jest-preset-angular';
+// tslint:disable
 import * as hammerjs from 'hammerjs';
+import 'jest-preset-angular';
 
 const mock = () => {
   let storage = {};
 
   return {
-    getItem: (key: string) => key in storage ? (storage as any)[key] : undefined,
-    setItem: (key: string, value: any) => (storage as any)[key] = value || '',
-    removeItem: (key: string) => delete (storage as any)[key],
-    clear: () => storage = {}
+    getItem: key => (key in storage ? (storage as any)[key] : undefined),
+    setItem: (key, value) => ((storage as any)[key] = value || ''),
+    removeItem: key => delete (storage as any)[key],
+    clear: () => (storage = {})
   };
 };
 
-Object.defineProperty(window, 'CSS', {value: mock()});
-Object.defineProperty(window, 'localStorage', {value: mock()});
-Object.defineProperty(window, 'sessionStorage', {value: mock()});
-Object.defineProperty(window, 'matchMedia', {value: jest.fn(() => ({matches: true}))});
-Object.defineProperty(document, 'doctype', {value: '<!DOCTYPE html>'});
+Object.defineProperty(window, 'CSS', { value: mock() });
+Object.defineProperty(window, 'localStorage', { value: mock() });
+Object.defineProperty(window, 'sessionStorage', { value: mock() });
+Object.defineProperty(window, 'matchMedia', { value: jest.fn(() => ({ matches: true })) });
+Object.defineProperty(document, 'doctype', { value: '<!DOCTYPE html>' });
 
 Object.defineProperty(window, 'getComputedStyle', {
   value: () => ({
@@ -25,13 +26,17 @@ Object.defineProperty(window, 'getComputedStyle', {
   })
 });
 
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ({
+    getPropertyValue: () => ''
+  })
+});
+
 Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true
-    };
-  }
+  value: () => ({
+    enumerable: true,
+    configurable: true
+  })
 });
 
 (window as any).Hammer = hammerjs;
